1d6746177a6b0ea0225b124463ef23e38ed65ca7
cbsilva <cpaulo99@hotmail.com>
2024-08-17 01:39:21 -0300

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,26 @@
+print("jornada de dados")
+
+#variaveis
+idade = 34
+nome = "Cleberson"
+print(nome)
+
+# Print
+print(idade)
+
+#soma
+
+nova_idade = idade + 1
+print(nova_idade)
+
+#Lista
+lista = [1,2,3,4,5]
+print(lista)
+
+#Métodos
+lista_invertida = lista[::-1]
+print(lista_invertida)
+
+##OUTRA FORMA DE LISTA INTERTRIDA
+lista_invertida_2 = list(reversed(lista))
+print(lista_invertida_2)
